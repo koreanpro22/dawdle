8834f2946078c293cd79fdb49757b4fbdc83fd45
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-08-27 21:38:57 -0400

    user auth is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,71 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
-import { getAllEmails, addEmail } from "./lib/actions";
-
-// export default function Component() {
-//   const { data: session } = useSession()
-//   console.log(session)
-//   if (session) {
-//     return (
-//       <>
-//         Signed in as {session.user.email} <br />
-//         <button onClick={() => signOut()}>Sign out</button>
-//       </>
-//     )
-//   }
-
+import { useState} from "react";
+import { auth } from "../lib/firebase/config";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
+import { selectUser } from "../lib/store/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../lib/store/userSlice";
 
 
 export default function Landing() {
-
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false); // State to toggle between login and signup
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
+  console.log('user:', user);
 
-    function isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+  const handleSubmit = () => {
+    if (isLogin) {
+      signInWithEmailAndPassword(auth, email, password).catch((error) => {
+        const errorMessage = error.message;
+        alert("Invalid Email or Password");
+      });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password).catch((error) => {
+        const errorMessage = error.message;
+        setError("Invalid Email or Password");
+      });
+    }
+  };
+
+  const handlePasswordReset = () => {
+    const email = prompt("Please Enter Email");
+    if (email) {
+      sendPasswordResetEmail(auth, email)
+        .then(() => alert("Email sent. Check inbox for password reset"))
+        .catch((error) => {
+          console.error(error);
+          setError("Failed to send reset email");
+        });
+    }
+  };
+
+  const handleSignOut = () => {
+    if (confirm("Are you sure you want to sign out?")) {
+      signOut(auth)
+        .then(() => {
+          dispatch(setUser(null));
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  };
+
+  if (user) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <button
+          className="bg-red-600 text-white text-3xl py-5 px-10 rounded-full"
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
+      </div>
+    );
   }
-
-  const handleEmailSubmit = async () => {
-    if (!isValidEmail(email)) return alert('Not a valid Email')
-    const res = await addEmail(email)
-    setEmail('')
-    if (res === 'Email already exists') return alert('Email already exists')
-    alert(`Set up email for ${email}`)
-  }
-
-  const handleGetAllEmails = async () => {
-    const emails = await getAllEmails()
-    console.log(emails)
-  }
-
 
   return (
     <div className="flex flex-col lg:flex-row w-[80%] m-auto lg:h-screen py-[1vh]">
@@ -46,15 +73,18 @@ export default function Landing() {
         <h1 className="animate-fadeIn text-[6vh] lg:text-[7vh] text-primary-text-color font-[900]">
           Connect, plan, and experience. All in one place.
         </h1>
-        <div className="w-[95%]  text-primary-accent-color font-bold -ml-[2vh] lg:ml-0 my-5">
+        <div className="w-[95%] text-primary-accent-color font-bold -ml-[2vh] lg:ml-0 my-5">
           <h2 className="lg:w-[100%] animate-fadeIn text-[10vh] lg:text-[20vh] tracking-[-0.1em] font-[900] lg:pl-5">
             10,000
           </h2>
           <p className="lg:w-[61%] animate-fadeIn text-right text-[3.25vh] lg:text-[4vh] font-[900] -mr-[2vh] lg:mr-0">users are Dawdling</p>
         </div>
         <div className="w-[80%] lg:my-0 lg:mx-0 mx-auto flex flex-col gap-3 my-5 pb-5">
-          <p className="text-primary-text-color lg:self-start self-end underline underline-offset-2 ">
-            Are you one of them?
+          <p
+            className="text-primary-text-color lg:self-start self-end underline underline-offset-2 cursor-pointer"
+            onClick={() => setIsLogin(!isLogin)} // Toggle between login and signup
+          >
+            {isLogin ? "Become one of them!" : "Are you one of them?"}
           </p>
           <div className="flex lg:flex-row flex-col justify-start lg:gap-[2vh] gap-[1vh]">
             <input
@@ -64,33 +94,29 @@ export default function Landing() {
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
             />
-            <button
-              className="animate-fadeIn self-end bg-primary-accent-color text-primary-text-color w-min py-3 px-5 rounded-full"
-              // type="submit"
-              onClick={() => handleEmailSubmit()}
-            >
-              submit
-            </button>
-          </div>
-          {/* Email + Password Inputs */}
-          {/* <div className="flex lg:flex-row flex-col justify-start lg:gap-[2vh] gap-[1vh]">
-            <input
-              className="animate-fadeIn outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
-              type="text"
-              placeholder="Email"
-            />
             <input
               className="animate-fadeIn outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
             />
             <button
               className="animate-fadeIn self-end bg-primary-accent-color text-primary-text-color w-min py-3 px-5 rounded-full"
               type="submit"
+              onClick={handleSubmit}
             >
-              submit
+              {isLogin ? "Login" : "Sign Up"}
             </button>
-          </div> */}
+          </div>
+          {isLogin && (
+            <button
+              className="mt-3 text-primary-text-color underline cursor-pointer"
+              onClick={handlePasswordReset}
+            >
+              Forgot Password?
+            </button>
+          )}
         </div>
       </div>
 
@@ -109,16 +135,15 @@ export default function Landing() {
             </div>
           </div>
           <div className="">
-          <Image
-            src="/images/landing-page.png"
-            width={0}
-            height={0}
-            alt="Picture of the author"
-            sizes="100vw"
-            className=" w-full h-auto rounded-xl"
-          />
+            <Image
+              src="/images/landing-page.png"
+              width={0}
+              height={0}
+              alt="Landing Page Image"
+              sizes="100vw"
+              className="w-full h-auto rounded-xl"
+            />
           </div>
-        
         </div>
       </div>
     </div>
