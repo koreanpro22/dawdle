fbbbc42d98531cf96dcf389519c5d5e190400437
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-09-05 10:43:20 -0400

    have the dawdles ordered trying to get it to stay on the dawdle that i am on after changes

--- app/components/UpcomingDwadlesButton.tsx
+++ app/components/UpcomingDwadlesButton.tsx
@@ -1,4 +1,3 @@
-// UpcomingDwadlesButton.js
 "use client";
 
 import { setCurEvent, selectCurEvent } from "@/lib/store/curEventSlice";
@@ -22,7 +21,7 @@ interface Event {
 }
 
 interface UpcomingDwadlesButtonProps {
-  events: Event[]; // Corrected to handle an array of Event objects
+  events: Event[];
 }
 
 export default function UpcomingDwadlesButton({
@@ -31,42 +30,40 @@ export default function UpcomingDwadlesButton({
   const dispatch = useDispatch();
   const curEvent = useSelector(selectCurEvent);
 
-  // Function to parse date and time into a single Date object
   const parseDateTime = (event: Event) => {
     return new Date(`${event.date}T${event.time}`);
   };
 
-  // Sorting events by datetime
-
+  const sortedEvents = [...events].sort((a, b) => {
+    return parseDateTime(a).getTime() - parseDateTime(b).getTime();
+  });
 
   return (
     <div className="flex flex-col justify-around items-center w-full gap-[1vh] pt-[10vh]">
       <h1 className="text-primary-accent-color text-start w-full text-[2.5vh] font-[900]">
         Upcoming Dawdles 🦆🐤
       </h1>
-      {events
-        
-        .map((event, index) => (
-          <div
-            onClick={() => dispatch(setCurEvent(event))}
-            key={index}
-            className="h-[4rem] relative bg-[#fff] text-primary-accent-color pl-[2vh] flex gap-[2vh] items-center justify-between w-full rounded-full text-[1.75vh]"
-          >
-            <span className="text-[2vh] font-bold">{event.title}</span>
-            <div className="flex">
-              <div className="absolute top-0 right-0 rounded-[3vh] flex h-full justify-center items-center">
-
+      {sortedEvents.map((event, index) => (
+        <div
+          onClick={() => dispatch(setCurEvent(event))}
+          key={index}
+          className="h-[4rem] relative bg-[#fff] text-primary-accent-color pl-[2vh] flex gap-[2vh] items-center justify-between w-full rounded-full text-[1.75vh]"
+        >
+          <span className="text-[2vh] font-bold">{event.title}</span>
+          <div className="flex">
+            <div className="absolute top-0 right-0 rounded-[3vh] flex h-full justify-center items-center">
               <span className="text-[2vh] font-bold">
                 {`${new Date(event.date).toLocaleDateString("en-US", {
                   month: "numeric",
                   day: "numeric",
-                })} ${new Date(
-                  `1970-01-01T${event.time}Z`
-                ).toLocaleTimeString("en-US", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  hour12: true,
-                })}`}
+                })} ${new Date(`1970-01-01T${event.time}Z`).toLocaleTimeString(
+                  "en-US",
+                  {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  }
+                )}`}
               </span>
               <div className="bg-[#360F50] rounded-[2.5vh] flex text-white justify-center items-center text-[1.75vh] gap-[1vh] px-[1vh] h-full">
                 <Image
@@ -78,11 +75,10 @@ export default function UpcomingDwadlesButton({
                 ></Image>
                 {event?.participants[0]?.email?.split("@")[0]}
               </div>
-              </div>
-
             </div>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 }
